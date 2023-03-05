74769a41ab5cf84208ab7a81e9853b5fa68f4a56
yenvy0512 <58163698+yenvy0512@users.noreply.github.com>
2023-03-05 21:23:35 +0700

    Update Topic_01_Check_Environment.java
    Test

--- src/webdriver/Topic_01_Check_Environment.java
+++ src/webdriver/Topic_01_Check_Environment.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -34,12 +34,12 @@ public class Topic_01_Check_Environment {
 //		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/kieuyenvy612");
+		driver.get("https://www.facebook.com/");
 	}
 
 	@Test
 	public void TC_01_Url() {
-		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/kieuyenvy612");
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
 	}
 
 	@Test
