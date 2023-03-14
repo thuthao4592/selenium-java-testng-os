29e0f126cbdad169a83e440a69d2c0514a3e55b0
yenvy0512 <58163698+yenvy0512@users.noreply.github.com>
2023-03-15 01:13:34 +0700

    Create Template.java
    Create Template

---
+++ src/webdriver/Template.java
@@ -0,0 +1,64 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Template {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+//		if (osName.contains("Windows")) {
+//			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+//		} else {
+//			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+//		}
+		if (osName.contains("Windows10")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		} else {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		}
+
+
+		driver = new ChromeDriver();
+//		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_01() {
+		
+	}
+
+	@Test
+	public void TC_02() {
+		
+	}
+
+	@Test
+	public void TC_03() {
+		
+	}
+	@Test
+	
+	public void TC_04() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
