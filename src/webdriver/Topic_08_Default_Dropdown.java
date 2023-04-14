771e716ca76d7eba59ba7f2c8ae9217f31538bc1
trinhthinga <thunga155tb@gmail.com>
2023-04-15 00:15:31 +0700

    Topic_08_Handle_Default_Dropdown

---
+++ src/webdriver/Topic_08_Default_Dropdown.java
@@ -0,0 +1,142 @@
+package webdriver;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_08_Default_Dropdown {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	String firstName, lastName, emailAddress, companyName, password, day, month, year;
+	String countryName, provinceName, cityName, addressName, postalCode, phoneNumber;
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		firstName = "Peter";
+		lastName = "Pan";
+		emailAddress = "peter" + getRandomNumber() + "@gmail.com";
+		companyName = "AMCCompany";
+		password = "123456";
+		day = "20";
+		month = "February";
+		year = "1999";
+		
+		countryName = "United States";
+		provinceName = "Florida";
+		cityName = "Miami";
+		addressName = "12 Clarm Street";
+		postalCode = "33111";
+		phoneNumber = "+13050035623";
+	}
+
+	@Test
+	public void TC_01_Register_New_Account() {
+		driver.get("https://demo.nopcommerce.com/");
+
+		driver.findElement(By.cssSelector("a.ico-register")).click();
+
+		driver.findElement(By.id("FirstName")).sendKeys(firstName);
+		driver.findElement(By.id("LastName")).sendKeys(lastName);
+
+		new Select(driver.findElement(By.name("DateOfBirthDay"))).selectByVisibleText(day);
+		new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText(month);
+		new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText(year);
+		
+
+		driver.findElement(By.id("Email")).sendKeys(emailAddress);
+		driver.findElement(By.id("Company")).sendKeys(companyName);
+		driver.findElement(By.id("Password")).sendKeys(password);
+		driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+
+		driver.findElement(By.id("register-button")).click();
+
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(), "Your registration completed");
+
+		driver.findElement(By.cssSelector("a.ico-login")).click();
+		driver.findElement(By.id("Email")).sendKeys(emailAddress);
+		driver.findElement(By.id("Password")).sendKeys(password);
+		driver.findElement(By.className("login-button")).click();
+
+		driver.findElement(By.cssSelector("a.ico-account")).click();
+
+		// Verify
+		Assert.assertEquals(driver.findElement(By.id("FirstName")).getAttribute("value"), firstName);
+		Assert.assertEquals(driver.findElement(By.id("LastName")).getAttribute("value"), lastName);
+		Assert.assertEquals(
+				new Select(driver.findElement(By.name("DateOfBirthDay"))).getFirstSelectedOption().getText(), day);
+		Assert.assertEquals(
+				new Select(driver.findElement(By.name("DateOfBirthMonth"))).getFirstSelectedOption().getText(), month);
+		Assert.assertEquals(
+				new Select(driver.findElement(By.name("DateOfBirthYear"))).getFirstSelectedOption().getText(), year);
+		Assert.assertEquals(driver.findElement(By.id("Email")).getAttribute("value"), emailAddress);
+		Assert.assertEquals(driver.findElement(By.id("Company")).getAttribute("value"), companyName);
+
+	}
+
+	@Test
+	public void TC_02_Add_Address() {
+		driver.findElement(By.cssSelector("li.customer-addresses>a")).click();
+		driver.findElement(By.cssSelector("button.add-address-button")).click();
+		
+		driver.findElement(By.id("Address_FirstName")).sendKeys(firstName);
+		driver.findElement(By.id("Address_LastName")).sendKeys(lastName);
+		driver.findElement(By.id("Address_Email")).sendKeys(emailAddress);
+		driver.findElement(By.id("Address_Company")).sendKeys(companyName);
+		new Select(driver.findElement(By.id("Address_CountryId"))).selectByVisibleText(countryName);
+		new Select(driver.findElement(By.id("Address_StateProvinceId"))).selectByVisibleText(provinceName);
+		driver.findElement(By.id("Address_City")).sendKeys(cityName);
+		driver.findElement(By.id("Address_Address1")).sendKeys(addressName);
+		driver.findElement(By.id("Address_ZipPostalCode")).sendKeys(postalCode);
+		driver.findElement(By.id("Address_PhoneNumber")).sendKeys(phoneNumber);
+		
+		driver.findElement(By.cssSelector("button.save-address-button")).click();
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.name")).getText(), firstName + " " + lastName);
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.email")).getText().contains(emailAddress));
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.phone")).getText().contains(phoneNumber));
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.company")).getText(), companyName);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.address1")).getText(), addressName);
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.city-state-zip")).getText().contains(cityName));
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.city-state-zip")).getText().contains(provinceName));
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.city-state-zip")).getText().contains(postalCode));
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.country")).getText(), countryName);
+
+		
+	
+	}
+
+	public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(99999);
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
