381d4bf4e9dc4262c8fabb9572cdd015dc7f7240
LA <lananhbka43@gmail.com>
2023-04-16 13:51:38 +0700

    LA commit clone code from master

--- src/webdriver/Topic_01_Check_Environment.java
+++ src/webdriver/Topic_01_Check_Environment.java
@@ -1,12 +1,9 @@
 package webdriver;
-import java.sql.Driver;
-//hangltt
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -20,69 +17,33 @@ public class Topic_01_Check_Environment {
 
 	@BeforeClass
 	public void beforeClass() {
-
 		if (osName.contains("Windows")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		} else {
-			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 		}
 
-//
-		driver = new ChromeDriver(); 
-//		driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		//mở trang register ra
-		driver.get("https://demo.nopcommerce.com/register");
-	}
-
-
-	@Test
-	public void TC_01_ID() {
-		driver.findElement(By.id("FirstName")).sendKeys("Automation");
+		driver.get("https://www.facebook.com/");
 	}
 
 	@Test
-	public void TC_02_Class() {
-		driver.get("https://demo.nopcommerce.com/search");
-		driver.findElement(By.className("search-text")).sendKeys("Macbook");
+	public void TC_01_Url() {
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
 	}
 
 	@Test
-	public void TC_03_Name() {
-		driver.findElement(By.name("advs")).click();
+	public void TC_02_Logo() {
+		Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
 	}
+
 	@Test
-	public void TC_04_TagName() {
-		System.out.print(driver.findElement(By.tagName("input")).getSize());
+	public void TC_03_Form() {
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 	}
-	
-	@Test
-	public void TC_05_Linktext() {
-	driver.findElement(By.linkText("Addresses")).click();
-	}
-	@Test
-	public void TC_06_TagName() {
-		driver.findElement(By.partialLinkText("vendor account")).click();
-	}
-	@Test
-	public void TC_07_Css() {
-		driver.get("https://demo.nopcommerce.com/register");
-        driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Selenium");
-        driver.findElement(By.cssSelector("input[id='LastName']")).sendKeys("Locator");
-        driver.findElement(By.cssSelector("input[name='Email']")).sendKeys("automation@gmail.com");
-        
-	}
-	@Test
-	public void TC_08_xpath() {
-		driver.get("https://demo.nopcommerce.com/register");
-        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("Selenium");
-        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("Locator");
-        driver.findElement(By.xpath("//label[text()='Email:']/following-sibling::input")).sendKeys("automation@gmail.com");
-        
-	}
-	
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
---
+++ src/webdriver/Topic_02_Xpath_Css.java
@@ -0,0 +1,13 @@
+package webdriver;
+
+public class Topic_02_Xpath_Css {
+	public void TC_01() {
+		// Update
+		// Delete
+		// Add
+		// ........
+	}
+	public void TC_02() {
+		
+	}
+}
