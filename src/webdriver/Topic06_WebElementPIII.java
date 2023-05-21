b9c88fe1b8dda9d6646b65214489ba29f78103ce
Thuy <honghtpt@gmail.com>
2023-05-21 17:15:47 +0700

    WebBrowser/ WebElement Exercise - Part II

--- src/webdriver/Topic06_WebElementPIII.java
+++ src/webdriver/Topic06_WebElementPIII.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -12,9 +13,10 @@ import org.testng.annotations.Test;
 
 public class Topic06_WebElementPIII {
 	WebDriver driver;
+	Random rand;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-	
+	String EmailAddress,firstName,LastName,FullName,passWord;
 	By emailTextBox = By.id("mail");
 	By ageUnder18Radio = By.xpath("//input[@id=\"under_18\"]");
 	By educationTextArea = By.xpath("//textarea[@id=\"edu\"]");
@@ -34,6 +36,11 @@ public class Topic06_WebElementPIII {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		EmailAddress="Automaiton"+ rand.nextInt(9999)+"@gmail.com";
+		firstName= "Automatiom";
+		LastName="FC";
+		FullName=firstName+" "+LastName;
+		passWord="123456a";
 		}
 	
 	@Test
@@ -77,11 +84,11 @@ public class Topic06_WebElementPIII {
 	}
 
 	@Test
-	public void TC_04_Incorrect_Email() {
+	public void TC_04_Incorrect_Email_Pass() {
 		driver.get("http://live.techpanda.org/");
 		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
 		sleepInSecond(3);
-		driver.findElement(By.id("email")).sendKeys("auto123@gmail.com");
+		driver.findElement(By.id("email")).sendKeys(EmailAddress);
 		driver.findElement(By.id("pass")).sendKeys("1236435");
 		driver.findElement(By.id("send2")).click();
 		sleepInSecond(3);
@@ -89,6 +96,48 @@ public class Topic06_WebElementPIII {
 		
 		
 		}
+	@Test
+	public void TC_05_Creat_New_ACC() {
+		driver.get("http://live.techpanda.org/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		sleepInSecond(3);
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='Create an Account']")).click();
+		sleepInSecond(3);
+		driver.findElement(By.id("firstname")).sendKeys(firstName);
+		driver.findElement(By.id("lastname	")).sendKeys(LastName);
+		driver.findElement(By.id("email_address")).sendKeys(EmailAddress);
+		driver.findElement(By.id("password")).sendKeys(passWord);
+		driver.findElement(By.id("confirmation")).sendKeys(passWord);
+		driver.findElement(By.xpath("//div[@class='footer']//button[@title='Register']")).click();
+
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.xpath("//ul[@class='messages']")).getText(), "Thank you for registering with Main Website Store.");
+		String ContactInformationText= driver.findElement(By.xpath("//h3[text()='Contact Information']/parent::div/following-sibling::div/p")).getText();
+		////h3[text()='Contact Information']/parent::div/following-sibling::div/p lay len div cha cua h3 roi follow sang em cua div cha vua tim, lay the p
+        System.out.println(ContactInformationText);		
+        Assert.assertTrue(ContactInformationText.contains(FullName));
+        Assert.assertTrue(ContactInformationText.contains(EmailAddress));
+		driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[text()='Account']")).click();
+		driver.findElement(By.xpath("//a[@title='Log Out']")).click();
+
+		sleepInSecond(5);
+		Assert.assertTrue(driver.findElement(By.xpath("//img[contains(@src,'logo.png')]")).isDisplayed());
+
+        }
+	@Test
+	public void TC_06_Creat_New_ACC() {
+		driver.get("http://live.techpanda.org/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		sleepInSecond(3);
+		driver.findElement(By.id("email")).sendKeys(EmailAddress);
+		driver.findElement(By.id("pass")).sendKeys("passWord");
+		driver.findElement(By.id("send2")).click();
+		sleepInSecond(3);
+		String ContactInformationText= driver.findElement(By.xpath("//h3[text()='Contact Information']/parent::div/following-sibling::div/p")).getText();
+		////h3[text()='Contact Information']/parent::div/following-sibling::div/p lay len div cha cua h3 roi follow sang em cua div cha vua tim, lay the p
+        System.out.println(ContactInformationText);		
+        Assert.assertTrue(ContactInformationText.contains(FullName));
+        Assert.assertTrue(ContactInformationText.contains(EmailAddress));		}
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
@@ -98,7 +147,6 @@ public class Topic06_WebElementPIII {
 	try {
 		Thread.sleep(timeinSecond* 1000);
 	} catch (InterruptedException e) {
-		
 		e.printStackTrace();
 	}
 	}
